484c2664739b9623950c5263eacc444ddb0d01f3
yeyan1996 <you@example.com>
2018-05-21 16:07:17 +0800

    'xxx'

--- src/home/home.vue
+++ src/home/home.vue
@@ -1,21 +1,18 @@
 <template>
   <div >
-  hello world
+<HomeHeader></HomeHeader>
   </div>
 </template>
 
 <script>
+import HomeHeader from "./components/header.vue"
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'home',
+components:{HomeHeader}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="stylus" scoped>
+<style scoped>
 
 </style>
