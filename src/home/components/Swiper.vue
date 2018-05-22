41f88e88ffab1a917cd94b46fd130c05419d030f
yeyan1996 <you@example.com>
2018-05-22 11:08:29 +0800

    'xxx'

---
+++ src/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="wrapper">
+<swiper :options="swiperOption">
+<!-- slides -->
+
+<swiper-slide   v-for="item in swiperlist" :key="item.id">         <!--循环语句写在swiper-slide上-->
+<img class="swiper-img" :src="item.urlimg"  >
+</swiper-slide>
+<div class="swiper-pagination"  slot="pagination"></div>
+</swiper>
+</div>
+</template>
+
+<script>
+export default {
+name: 'HomeSwiper',
+data () {
+return {
+swiperlist: [{
+id: '001',
+urlimg: '//img1.qunarzz.com/vc/c1/c3/4f/7cd9da5aadf1c5b15cdbe11424.jpg'
+}, {
+id: '002',
+urlimg: '//img1.qunarzz.com/vc/18/ec/c3/f51767d45459eae72cf32d9b37.jpg'
+}],
+swiperOption: {
+pagination: {
+el: '.swiper-pagination'
+},
+loop: true
+}
+}
+}
+
+}
+
+</script>
+<style lang="stylus"  scoped>
+.wrapper >>>.swiper-pagination-bullet-active
+ background : white;
+.swiper-img
+ width:100%;
+.wrapper
+ width:100%
+ height:0;
+ overflow:hidden;
+ padding-bottom:30.39%;
+ background :grey;
+
+
+</style>
--- src/home/home.vue
+++ src/home/home.vue
@@ -1,14 +1,19 @@
 <template>
   <div >
 <HomeHeader></HomeHeader>
+  <HomeSwiper></HomeSwiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from "./components/header.vue"
+import HomeSwiper from "./components/Swiper.vue"
 export default {
   name: 'home',
-components:{HomeHeader}
+components:{
+  HomeHeader,
+  HomeSwiper
+  }
 }
 </script>
 
