d9c8efd8e7e9d655f2f4ee67be88df87ac16ba35
yeyan1996 <you@example.com>
2018-05-24 22:06:33 +0800

    'city.search'

--- src/city/city.vue
+++ src/city/city.vue
@@ -1,14 +1,19 @@
 <template>
+<div>
 <cityHeader></cityHeader>
+<citySearch></citySearch>
+</div>
 </template>
+
 <script>
 import cityHeader from "./components/header.vue"
-
+import citySearch from "./components/search.vue"
 
 export default{
 name:"city",
 components:{
-cityHeader
+cityHeader,
+citySearch
 }
 }
 </script>
---
+++ src/city/components/search.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="search">
+<input class="searchinput"  placeholder="请输入城市" />
+</div>
+</template>
+<script>
+
+
+
+export default {
+name: "citySearch"
+
+
+}
+
+</script>
+
+<style lang="stylus" scoped>
+@import "~styles/varibles.styl"
+.search{
+ background :$bgColor;
+ height:$headerheight;
+ padding:0 .15rem;
+}
+.searchinput{
+ padding:.1rem
+ text-align:center;
+ box-sizing:border-box;
+ height:.6rem;
+ width:100%;
+ background:white;
+}
+</style>
